fd6ec39d778ffc9723a2fc4331aca51ae04473ee
瀬戸口理美 <setoguchisatomi@setoguchisatominoiMac.local>
2024-08-04 12:44:50 +0900

    課題8の提出

--- app/models/embed.rb
+++ app/models/embed.rb
@@ -13,7 +13,15 @@ class Embed < ApplicationRecord
   has_one :article_block, as: :blockable, dependent: :destroy
   has_one :article, through: :article_block
 
-  enum embed_type: { youtube: 0 }
+  enum embed_type: { youtube: 0, x: 1 }
 
   validates :identifier, length: { maximum: 200 }
+
+  def split_id_from_youtube_url
+    identifier.split('/').last if youtube?
+  end
+
+  def split_id_from_x_url
+    identifier.split('/',4).last if x?
+  end  
 end
